65140936ccad89fdc4c0fa4677351146779db3d5
신동규 <donggyu9410@gmail.com>
2021-11-17 19:14:24 +0900

    프로젝트 생성

---
+++ twitter/twitter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  twitter
+//
+//  Created by 신동규 on 2021/11/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ twitter/twitter/twitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  twitterApp.swift
+//  twitter
+//
+//  Created by 신동규 on 2021/11/17.
+//
+
+import SwiftUI
+
+@main
+struct twitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
