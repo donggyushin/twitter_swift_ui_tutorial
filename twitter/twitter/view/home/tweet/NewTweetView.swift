932573bcbddffc4068990c62b85b3b58d9fd2952
신동규 <donggyu9410@gmail.com>
2021-11-19 20:29:22 +0900

    feat: NewTweet 작업중
    NewTweetView 페이지 레이아웃 작업

---
+++ twitter/twitter/view/home/tweet/NewTweetView.swift
@@ -0,0 +1,55 @@
+//
+//  NewTweetView.swift
+//  twitter
+//
+//  Created by 신동규 on 2021/11/19.
+//
+
+import SwiftUI
+
+struct NewTweetView: View {
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                HStack() {
+                    Image("batman")
+                        .resizable()
+                        .frame(width: 56, height: 56)
+                        .clipShape(Circle())
+                        .scaledToFill()
+                        .padding(.leading)
+                    
+                    Text("Hello World!")
+                        .foregroundColor(.gray)
+                    
+                    Spacer()
+                }
+                
+                Spacer()
+            }
+            
+            .navigationBarItems(
+                leading: Button(action: {
+                
+            }, label: {
+                Text("Cancel")
+            }), trailing: Button(action: {
+                
+            }, label: {
+                Text("Tweet")
+                    .padding(.horizontal)
+                    .padding(.vertical, 8)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .clipShape(Capsule())
+            }))
+        }
+    }
+}
+
+struct NewTweetView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewTweetView()
+    }
+}
