3814949a27b6906e4c64d06947573569aa53a66b
신동규 <donggyu9410@gmail.com>
2021-11-20 14:36:27 +0900

    feat: authentication 작업중
    Registration View 레이아웃 제작

--- twitter/twitter/view/authentication/RegistrationView.swift
+++ twitter/twitter/view/authentication/RegistrationView.swift
@@ -1,17 +1,19 @@
 //
-//  LoginView.swift
+//  RegistrationView.swift
 //  twitter
 //
 //  Created by 신동규 on 2021/11/20.
 //
 
 import SwiftUI
-import UIKit
 
-struct LoginView: View {
+struct RegistrationView: View {
     
+    @State var fullName: String = ""
     @State var email: String = ""
+    @State var userName: String = ""
     @State var password: String = ""
+    
     let screenWidth = UIScreen.main.bounds.width
     
     var body: some View {
@@ -21,50 +23,46 @@ struct LoginView: View {
                     .resizable()
                     .scaledToFit()
                     .frame(width: 120)
-                   
+                
+                TextFieldView(placeHolder: "Full Name", image_name: "person", text: $fullName)
+                    .padding(.horizontal)
+                
                 TextFieldView(placeHolder: "Email", image_name: "envelope", text: $email)
-                    .padding()
+                    .padding(.horizontal)
+                
+                TextFieldView(placeHolder: "User Name", image_name: "person", text: $userName)
+                    .padding(.horizontal)
                 
                 PasswordTextFieldView(placeHolder: "Password", text: $password)
                     .padding(.horizontal)
                 
-                HStack {
-                    Spacer()
-                    Button {
-                        
-                    } label: {
-                        Text("Forgot Password?")
-                            .foregroundColor(.white)
-                            .font(.footnote)
-                    }
-                    .padding()
-                }
-                
                 Button {
                     
                 } label: {
-                    Text("Sign In")
+                    Text("Sign Up")
                         .frame(width: screenWidth - 40, height: 40)
                         .background(.white)
                         .cornerRadius(20)
                 }
-
+                .padding(.top, 40)
+                
                 Spacer()
                 
                 Button {
                     
                 } label: {
-                    Text("Don't have an account?")
+                    Text("Already have an account")
                         .foregroundColor(.white)
                 }
             }
-            .background(background_color)
+            
+                .background(background_color)
         }
     }
 }
 
-struct LoginView_Previews: PreviewProvider {
+struct RegistrationView_Previews: PreviewProvider {
     static var previews: some View {
-        LoginView()
+        RegistrationView()
     }
 }
