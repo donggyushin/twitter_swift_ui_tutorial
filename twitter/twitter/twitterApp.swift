4d784fe5bdac4a7534c249a6fc5af9b38d178e40
신동규 <donggyu9410@gmail.com>
2021-11-22 20:59:41 +0900

    feat: Firebase App 추가 작업중
    FirebaseApp 초기화 코드 추가

--- twitter/twitter/twitterApp.swift
+++ twitter/twitter/twitterApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct twitterApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
 //            ContentView()
