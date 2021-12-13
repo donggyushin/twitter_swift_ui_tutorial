8cf769b1031d1c3261417a4e68a11985ba0abd92
신동규 <donggyu9410@gmail.com>
2021-12-13 19:48:14 +0900

    feat: 유저별 트윗 불러오는 기능 추가

--- twitter/twitter/viewModel/ProfileViewModel.swift
+++ twitter/twitter/viewModel/ProfileViewModel.swift
@@ -10,11 +10,16 @@ import Firebase
 
 class ProfileViewModel: ObservableObject {
     @Published var isFollowed = false
+    @Published var userTweets: [Tweet] = []
+    @Published var likedTweets: [Tweet] = []
+    
     let user: TwitterUser
     
     init(user: TwitterUser) {
         self.user = user
         checkIfUserIsFollowed()
+        fetchUserTweets()
+        fetchLikedTweets()
     }
     
     func follow() {
@@ -45,6 +50,26 @@ class ProfileViewModel: ObservableObject {
         followingRef.document(user.id).getDocument { snapshot, _ in
             guard let isFollowed = snapshot?.exists else { return }
             self.isFollowed = isFollowed
+        }
+    }
+    
+    func fetchUserTweets() {
+        COLLECTION_TWEETS.whereField("uid", isEqualTo: user.id).getDocuments { snapshot, _ in
+            guard let documents = snapshot?.documents else { return }
+            self.userTweets = documents.map({ .init(dictionary: $0.data()) })
+        }
+    }
+    
+    func fetchLikedTweets() {
+        COLLECTION_USERS.document(user.id).collection("user-likes").getDocuments { snapshot, _ in
+            guard let documents = snapshot?.documents else { return }
+            let tweetIds = documents.map({ $0.documentID })
+            tweetIds.forEach({
+                COLLECTION_TWEETS.document($0).getDocument { snapshot, _ in
+                    guard let data = snapshot?.data() else { return }
+                    self.likedTweets.append(.init(dictionary: data))
+                }
+            })
         }
     }
 }
